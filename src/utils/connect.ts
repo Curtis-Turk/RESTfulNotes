7cdd0456f825ecf38df02a8cdaa98f4745fd6756
Curtis Turk <curtis.c.turk@outlook.com>
2022-11-23 14:35:30 +0000

    added a logger to replace simple console log

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import config from "config";
+import log from "./logger";
 
 export default async function connect() {
   const dbUri = config.get<string>("dbUri");
   try {
     await mongoose.connect(dbUri);
-    console.log("Connected to DB");
+    log.info("Connected to DB");
   } catch (err) {
-    console.log("Could not connect to DB");
+    log.info("Could not connect to DB");
     process.exit(1);
   }
 }
